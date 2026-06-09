ef839225bafe7adb19296f438fb8a67a713fddde
Arowolo Godwin <arowologodwin4@gmail.com>
2026-06-09 20:52:51 +0100

    Switch to Render deployment

--- run.py
+++ run.py
@@ -4,5 +4,6 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
+    port  = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "False") == "True"
-    app.run(debug=debug, host="0.0.0.0", port=5000)
+    app.run(debug=debug, host="0.0.0.0", port=port)
